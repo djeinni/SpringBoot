a0cc9173e7289461f092adb5c954ec63bdb299d3
Ba <djeinaba.ba@accenture.com>
2024-02-05 21:49:28 +0100

    first commit

---
+++ src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,15 @@
+package com.pluralsight.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FundamentalsApplication.class, args);
+		System.out.println("Spring boot Application");
+	}
+
+}
